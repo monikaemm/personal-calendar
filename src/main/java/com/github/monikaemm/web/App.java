f82d96e56cefd6dd1890da62f6c5f585099e0f13
monikaemm <monika.miluch@gmail.com>
2015-07-10 20:02:12 +0200

    Exported current week view in ReST api.

--- src/main/java/com/github/monikaemm/web/App.java
+++ src/main/java/com/github/monikaemm/web/App.java
@@ -1,13 +1,25 @@
 package com.github.monikaemm.web;
 
-import static spark.Spark.*;
+import com.github.monikaemm.calendar.WeekView;
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 /**
  * Created by MiluchM on 2015-06-25.
  */
 public class App {
+    static Gson gson = new Gson();
     public static void main(String[] args) {
+        WeekView week = new WeekView();
+
         staticFileLocation("app");
-        get("/hello", (req, res) -> "Monika");
+        get("/currentWeek", (req, res) -> week.getDays(), toJson());
+    }
+
+    private static ResponseTransformer toJson() {
+        return o -> gson.toJson(o);
     }
 }
