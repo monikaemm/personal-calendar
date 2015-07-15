861dd78c7ee4b70699f35248d6218b226a7980c8
monikaemm <monika.miluch@gmail.com>
2015-07-15 20:17:57 +0200

    Added dynamic list.

--- src/main/resources/app/script.js
+++ src/main/resources/app/script.js
@@ -1,5 +1,17 @@
 var app = angular.module("personal-calendar", []);
 app.controller("CalendarCtrl", function ($scope, $http) {
+  $scope.showTasks = function (day) {
+    if ($scope.tasksDay != day.dayOfWeek) {
+      $scope.tasksVisible = true;
+      $scope.tasksDay = day.dayOfWeek;
+      $scope.tasks = ["jedno zadanie", "drugie zadanie"];
+    }
+    else {
+      $scope.tasksVisible = false;
+      $scope.tasksDay = undefined;
+    }
+  };
+
   $http
     .get("/currentWeek")
     .success(function (result) {
