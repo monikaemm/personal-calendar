36f2b270d186fa26e52edcb2bf36c47b22737673
monikaemm <monika.miluch@gmail.com>
2015-07-26 15:16:45 +0200

    Searching tasks in web service.

--- src/main/java/com/github/monikaemm/web/App.java
+++ src/main/java/com/github/monikaemm/web/App.java
@@ -1,8 +1,16 @@
 package com.github.monikaemm.web;
 
+import com.github.monikaemm.calendar.TasksRepository;
 import com.github.monikaemm.calendar.WeekView;
 import com.google.gson.Gson;
+import spark.Request;
 import spark.ResponseTransformer;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
@@ -12,11 +20,29 @@ import static spark.Spark.staticFileLocation;
  */
 public class App {
     static Gson gson = new Gson();
+    static TasksRepository tasksRepo = new TasksRepository();
+
     public static void main(String[] args) {
         WeekView week = new WeekView();
+        tasksRepo.addTask(LocalDate.now(), "zadanie");
+        tasksRepo.addTask(LocalDate.now(), "zadanie 2 ");
+        tasksRepo.addTask(LocalDate.now(), "zadanie 3");
 
         staticFileLocation("app");
         get("/currentWeek", (req, res) -> week.getDays(), toJson());
+        get("/task", (req, res) -> getTasks(req), toJson());
+    }
+
+    private static List<String> getTasks(Request req) {
+        String dayFilter = req.queryParams("day");
+        LocalDate requestDay = getDateInCurrentMonth(dayFilter);
+        return tasksRepo.getTasksFor(requestDay);
+    }
+
+    private static LocalDate getDateInCurrentMonth(String dayString) {
+        int dayOfMonth = Integer.parseInt(dayString);
+        LocalDate currentDate = LocalDate.now();
+        return currentDate.withDayOfMonth(dayOfMonth);
     }
 
     private static ResponseTransformer toJson() {
